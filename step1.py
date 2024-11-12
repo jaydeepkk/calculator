36cb1df5e7a09e694c81a125a91867a67a14b447
Jaydeep K <jaydeep.karkare@gmail.com>
2024-11-12 13:56:13 +0000

    Step1 solved

---
+++ step1.py
@@ -0,0 +1,12 @@
+operator = input("Please enter the operator")
+num1 = int(input("PLease enter the first number"))
+num2= int(input("Please enter the second number"))
+if operator== "+":
+  output = num1+num2
+if operator== "-":
+  output = num1- num2
+if operator== "*":
+  output = num1* num2
+if operator== "/":
+  output = num1//num2
+print (output)
